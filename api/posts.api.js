b5093e332e871d0d48c1818e0a005e72eab48676
nivebaby <nivethikagnanaseelan@gmail.com>
2021-04-13 11:36:57 +0530

    lab5 posts.api.js

---
+++ api/posts.api.js
@@ -0,0 +1,30 @@
+const uuid=require('uuid');
+
+let posts=new Map();
+
+let createPost=(obj)=>{
+   let post={
+       name:obj.name,
+       description:obj.description,
+       id:uuid.v4(),
+       date:new Date()
+   }
+   posts.set(post.id,post);
+}
+
+
+let getPosts=()=>{
+    return [...posts.values()];
+}
+let getPost=(id)=>{
+    return posts.get(id);
+}
+
+module.exports={
+    createPost,
+    getPosts,
+    getPost
+};
+
+
+

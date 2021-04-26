1aa8f0bb2a0b594068b5a4d7f8eca64328f9e9b1
NivethikaGnanaseelan <68712025+NivethikaGnanaseelan@users.noreply.github.com>
2021-04-27 03:24:02 +0530

    Update posts.api.js

--- api/posts.api.js
+++ api/posts.api.js
@@ -2,14 +2,18 @@ const uuid=require('uuid');
 
 let posts=new Map();
 
-let createPost=(obj)=>{
-   let post={
-       name:obj.name,
-       description:obj.description,
-       id:uuid.v4(),
-       date:new Date()
-   }
+const createPost=({name,description})=>
+{
+    let post= {
+        id: uuid.v4(),
+        name,
+        description,
+        date: new Date()
+    }
+
+
    posts.set(post.id,post);
+    return post;
 }
 
 
